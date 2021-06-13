39540a2353e67aff1fac49ce54c6a8e864d5baf7
ratchathorn2545 <78552921+ratchathorn2545@users.noreply.github.com>
2021-06-13 17:17:31 +0700

    Add files via upload

---
+++ FinalTest_5.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int numitem, n;
+        string itemname, itemtype;
+        Console.Write("Number of item in bag; ");
+        n = int.Parse(Console.ReadLine());
+
+        for (numitem = 1; numitem <= n; numitem++) ;
+        {
+            Console.WriteLine("Input Item: ");
+            itemname = Console.ReadLine();
+            Console.WriteLine("Input Type: ");
+            itemtype = Console.ReadLine();
+        }
+        Console.WriteLine("Input ShowAll or Itemtype: ");
+        string showall = Console.ReadLine();
+
+        if (showall == "ShowAll")
+        {
+            Console.Write(" ");
+            Console.WriteLine();
+        }
+    }
+}
+
+
+   
+
